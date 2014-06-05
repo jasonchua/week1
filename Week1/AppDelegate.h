1231e894557d42a780bff90069297b8de889c7ae
Jason Chua <Jason@Jasons-MacBook-Pro-2.local>
2014-06-04 22:01:16 -0700

    Initial Commit

---
+++ Week1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Week1
+//
+//  Created by Jason Chua on 6/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Week1
+//
+//  Created by Jason Chua on 6/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
